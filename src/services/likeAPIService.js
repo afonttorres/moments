a86b4bea25144ba2a467e883d50c039134dd98e6
Agnès Font Torres <afonttorres@gmail.com>
2022-08-22 14:22:14 +0200

    Likes & Saves

--- src/services/likeAPIService.js
+++ src/services/likeAPIService.js
@@ -1,10 +1,35 @@
 import axios from 'axios';
-import { authUtil } from '../utils/auth';
-const baseUrl = "http://localhost:8080";
+import { AuthService } from './AuthService';
+
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.withCredentials = false;
+axios.interceptors.request.use(function (config) {
+    const token = AuthService.getAuth().token;
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+});
+
+
+// axios.interceptors.response.use(
+//     function (response) {
+//       // CODE Executes in HTTP Status 2XX response
+//       // You Code Is IMPORTANT Here!
+//       console.log(response);
+//       return response;
+//     },
+//     function (error) {
+//       // CODE Executes in no HTTP Status 2XX response
+//       // You Code Is IMPORTANT Here!
+//       console.log(error.response);
+//       return Promise.reject(error);
+//     }
+//   );
 
 export const likeAPIService = {
     like(id) {
-        const like = axios.post(`${baseUrl}/likes`, { momentId: id, likerId: parseInt(authUtil.getLoggedUser()) })
+        const like = axios.post(`moments/${id}/likes`)
             .then(res => {
                 return res.data;
             })
--- src/services/saveAPIService.js
+++ src/services/saveAPIService.js
@@ -1,10 +1,35 @@
 import axios from 'axios';
-import { authUtil } from '../utils/auth';
-const baseUrl = "http://localhost:8080";
+import { AuthService } from './AuthService';
+
+axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.withCredentials = false;
+axios.interceptors.request.use(function (config) {
+    const token = AuthService.getAuth().token;
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+});
+
+
+// axios.interceptors.response.use(
+//     function (response) {
+//       // CODE Executes in HTTP Status 2XX response
+//       // You Code Is IMPORTANT Here!
+//       console.log(response);
+//       return response;
+//     },
+//     function (error) {
+//       // CODE Executes in no HTTP Status 2XX response
+//       // You Code Is IMPORTANT Here!
+//       console.log(error.response);
+//       return Promise.reject(error);
+//     }
+//   );
 
 export const saveAPIService = {
     save(id) {
-        const save = axios.post(`${baseUrl}/saves`, { momentId: id, saverId: parseInt(authUtil.getLoggedUser()) })
+        const save = axios.post(`moments/${id}/saves`)
             .then(res => {
                 return res.data;
             })
